7923c65e81d73edd0730a5b52985cc6855c485ca
Stepan <kaskader2001@mail.ru>
2023-10-23 11:28:17 +0300

    добвил форму контактов для интеграции с плагином WP

--- home.php
+++ home.php
@@ -23,24 +23,27 @@ TempLate Name: home
    <section>
       <h3>Редактирование записей</h2>
       <div class="block__container">
+
+      <!-- запускается цикл вывода постов -->
             <?php
                global $post;
 
                $myposts = get_posts([
                   'numberposts' => -1
                ]);
-
+      // проверяется, есть ли посты в вордпрессе
                if ($myposts) {
                   foreach( $myposts as $post) {
                         setup_postdata($post);
                         ?>
-
+      <!-- запускается сам цикл по шаблону ниже -->
                         <div class="block">
+                           <!-- изображения, где array - атрибуты и свойства изображения -->
                            <?php the_post_thumbnail(
                               array(550, 300),
                               array('class' => 'block__img')
                            ); ?>
-                          
+                          <!-- сам контент (заголовок и текст основной) -->
                            <div>
                               <h3><?php the_title(); ?></h3>
                               <p><?php the_content(); ?></p>
@@ -54,6 +57,14 @@ TempLate Name: home
                wp_reset_postdata(); 
             ?>
       </div>
+   </section>
+   <section>
+      <form>
+         <h2>Контакты</h2>
+         <input class="contact__input" type="text" placeholder="Имя">
+         <input class="contact__input" type="tel" placeholder="Телефон">
+         <button class="contact__btn" type="submit">Отправить</button>
+      </form>
    </section>
    </div>
 
