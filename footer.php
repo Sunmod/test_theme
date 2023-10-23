7ad3cb458685ab6ebc4ba52463488d3e41f6bbde
Stepan <kaskader2001@mail.ru>
2023-10-23 12:39:18 +0300

    добавил испровизированные иконки соц сетей для редактирования их через админку WP

--- footer.php
+++ footer.php
@@ -1,9 +1,18 @@
-<footer>
-        <nav>
-            <a href="#1">Footer link 1</a>
-            <a href="#2">Footer link 2</a>
-            <a href="#3">Footer link 3</a>
-        </nav>
-    </footer>
+    <section>
+        <div class="links__block">
+                <a class="instagram" href="#"></a>
+                <a class="telegram" href="#"></a>
+                <a class="google" href="#"></a>
+        </div>
+        <footer class="footer__container">
+            <?php the_custom_logo();?>
+            <nav>
+                <a href="#1">Footer link 1</a>
+                <a href="#2">Footer link 2</a>
+                <a href="#3">Footer link 3</a>
+            </nav>
+            <a class="logo_tel" href="tel:<?php the_field('phone_work ');?>"><?php the_field('phone');?></a>
+        </footer>
+    </section>
 </body>
 </html>
